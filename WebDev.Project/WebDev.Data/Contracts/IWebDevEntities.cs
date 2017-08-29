69eb24f948c6f0289f53fe21bf6fee6b6da3e3d7
Branimir123 <brani_svetoslavov@abv.bg>
2017-08-29 16:04:41 +0300

    Rapidly bootstraped models and data projects

---
+++ WebDev.Project/WebDev.Data/Contracts/IWebDevEntities.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace WebDev.Data.Contracts
+{
+    public interface IWebDevEntities
+    {
+        IDbSet<TEntity> DbSet<TEntity>()
+         where TEntity : class;
+
+        int SaveChanges();
+
+        void SetAdded<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetDeleted<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetUpdated<TEntry>(TEntry entity)
+            where TEntry : class;
+    }
+}
---
+++ WebDev.Project/WebDev.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WebDev.Data.Contracts;
+
+namespace WebDev.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IWebDevEntities dbContext;
+
+        public UnitOfWork(IWebDevEntities context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("Context cannot be null");
+            }
+
+            this.dbContext = context;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
