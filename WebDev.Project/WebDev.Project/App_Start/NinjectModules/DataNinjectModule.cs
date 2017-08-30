d8a11902109f5374fb35ba497d8793afa4e5095d
Branimir123 <brani_svetoslavov@abv.bg>
2017-08-30 11:26:01 +0300

    Installed all needed ninject packages and created two ninject modules

---
+++ WebDev.Project/WebDev.Project/App_Start/NinjectModules/DataNinjectModule.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.Modules;
+using Ninject.Web.Common;
+using PhotoLife.Data;
+using WebDev.Data;
+using WebDev.Data.Contracts;
+
+namespace WebDev.Project.App_Start.NinjectModules
+{
+    public class DataNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IWebDevEntities>().To<WebDevEntities>().InRequestScope();
+            this.Bind<IUnitOfWork>().To<UnitOfWork>();
+            this.Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
+        }
+    }
+}
---
+++ WebDev.Project/WebDev.Project/App_Start/NinjectModules/ServicesNinjectModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+using WebDev.Services;
+using WebDev.Services.Contracts;
+
+namespace WebDev.Project.App_Start.NinjectModules
+{
+    public class ServicesNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IUserService>().To<UserService>();
+            this.Bind<ITopicService>().To<TopicService>();
+        }
+    }
+}
