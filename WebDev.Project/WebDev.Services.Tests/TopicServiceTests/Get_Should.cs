4a43c2a3026fa24d2d8ca41a858c1790d4b8bfe1
Branimir123 <brani_svetoslavov@abv.bg>
2017-08-30 14:01:43 +0300

    Add tests for newly created TopicService method

---
+++ WebDev.Project/WebDev.Services.Tests/TopicServiceTests/Get_Should.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using WebDev.Data.Contracts;
+using WebDev.Models;
+
+namespace WebDev.Services.Tests.UserServiceTests
+{
+    [TestFixture]
+    public class Get_Should
+    {
+
+        [TestCase(1, 10, "name", "asc")]
+        [TestCase(1, 10, "name", "desc")]
+        [TestCase(1, 10, "", "")]
+        public void _Call_Repository_GetAll_Property_Exactly_Once(
+            int page,
+            int size,
+            string sortBy,
+            string orderBy)
+        {
+            //Arrange
+            var mockedTopicRepository = new Mock<IRepository<Topic>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            
+            //Act
+            var service = new TopicService(mockedTopicRepository.Object, mockedUnitOfWork.Object);
+            service.Get(page, size, sortBy, orderBy);
+
+            //Assert
+            mockedTopicRepository.Verify(r => r.GetAll, Times.Once);
+        }
+    }
+}
