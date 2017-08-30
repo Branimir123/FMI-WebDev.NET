5790cfad8b0a7297f360ae06e69fff8869884258
Branimir123 <brani_svetoslavov@abv.bg>
2017-08-30 12:50:41 +0300

    Finish UnitTests

---
+++ WebDev.Project/WebDev.Services.Tests/TopicServiceTests/GetTopicById_Should.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using WebDev.Data.Contracts;
+using WebDev.Models;
+
+namespace WebDev.Services.Tests.TopicServiceTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+
+        [TestCase(10)]
+        [TestCase(15)]
+        [TestCase(50)]
+        public void _Call_Repository_GetById_Method_Exactly_Once(int id)
+        {
+            //Arrange
+            var mockedTopicRepository = new Mock<IRepository<Topic>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act
+            var service = new TopicService(mockedTopicRepository.Object, mockedUnitOfWork.Object);
+            service.GetTopicById(id);
+
+            //Assert
+            mockedTopicRepository.Verify(r => r.GetById(id), Times.Once);
+        }
+    }
+}
---
+++ WebDev.Project/WebDev.Services.Tests/TopicServiceTests/GetTopics_Should.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using WebDev.Data.Contracts;
+using WebDev.Models;
+
+namespace WebDev.Services.Tests.TopicServiceTests
+{
+    [TestFixture]
+    public class GetTopicById_Should
+    {
+
+        [Test]
+        public void _Call_Repository_GetAll_Property_Exactly_Once()
+        {
+            //Arrange
+            var mockedTopicRepository = new Mock<IRepository<Topic>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act
+            var service = new TopicService(mockedTopicRepository.Object, mockedUnitOfWork.Object);
+            service.GetTopics();
+
+            //Assert
+            mockedTopicRepository.Verify(r => r.GetAll, Times.Once);
+        }
+    }
+}
