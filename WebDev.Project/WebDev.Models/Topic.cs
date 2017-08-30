76c123c0aed090e45f2e3921a1dc93f96f7a4ac4
Branimir123 <brani_svetoslavov@abv.bg>
2017-08-30 10:48:29 +0300

    Add Topic Model

---
+++ WebDev.Project/WebDev.Models/Topic.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebDev.Models
+{
+    public class Topic
+    {
+        public Topic() { }
+        public Topic(string name, string word, User author)
+        {
+            this.Name = name;
+            this.Word = word;
+            this.Author = author;
+        }
+
+        [Key]
+        public int TopicId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Word { get; set; }
+
+        [ForeignKey("Author")]
+        public string UserId { get; set; }
+
+        public virtual User Author { get; set; }
+    }
+}
